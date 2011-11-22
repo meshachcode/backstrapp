69be111c143e1bdf481f1cf45eb88176144dd721
Meshach Jackson <meshachjackson@gmail.com>
2011-11-21 20:24:27 -0600

    testing r.js build

--- src/js/app.build.js
+++ src/js/app.build.js
@@ -13,7 +13,11 @@
     },
 	modules: [
         {
-            include: ["requireLib", "main"]
+            include: ["requireLib", "main"],
+            //True tells the optimizer it is OK to create
+            //a new file foo.js. Normally the optimizer
+            //wants foo.js to exist in the source directory.
+            create: true
         }
     ]
 })
