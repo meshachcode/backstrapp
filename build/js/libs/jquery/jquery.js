62987e38f461d1fac645eb744493a2a38d826bb0
Meshach Jackson <meshachjackson@gmail.com>
2011-11-21 20:05:12 -0600

    testing r.js build

---
+++ build/js/libs/jquery/jquery.js
@@ -0,0 +1,1 @@
+define(["loader"],function(a){return a.$})
---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+require.config({paths:{loader:"libs/backbone/loader",jQuery:"libs/jquery/jquery",Underscore:"libs/underscore/underscore",Backbone:"libs/backbone/backbone",templates:"../templates"}}),require(["app"],function(a){a.initialize()})
