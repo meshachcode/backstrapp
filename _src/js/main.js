3709c63fd18151bc5ed04d46b167f47e5635e449
Meshach Jackson <meshachjackson@gmail.com>
2011-11-29 18:33:51 -0600

    slow updates to structure

--- _src/js/main.js
+++ _src/js/main.js
@@ -7,6 +7,7 @@ require.config({
   paths: {
     loader: 'libs/backbone/loader',
     jQuery: 'libs/jquery/jquery',
+    Handlebars: 'libs/handlebars/handlebars',
     Underscore: 'libs/underscore/underscore',
     Backbone: 'libs/backbone/backbone',
     templates: '../templates'
@@ -17,12 +18,18 @@ require.config({
 require([
 
   // Load our app module and pass it to our definition function
-  'app'
+  'views/app/view',
+  'libs/debug',
 
   // Some plugins have to be loaded in order due to their non AMD compliance
   // Because these scripts are not "modules" they do not pass any values to the definition function below
-], function(App){
+  'order!libs/jquery/jquery-min',
+  'order!libs/handlebars/handlebars-min',
+  'order!libs/underscore/underscore-min',
+  'order!libs/backbone/backbone-min',
+
+], function(AppView){
   // The "app" dependency is passed in as "App"
   // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
-  App.initialize();
+  var app = new AppView();
 });
