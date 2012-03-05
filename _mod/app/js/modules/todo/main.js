dc929a8d64af7590fa0ec993e814a877aa968c73
meshachj <meshachjackson@gmail.com>
2012-03-04 18:56:09 -0600

    creating test.html

---
+++ _mod/app/js/modules/todo/main.js
@@ -0,0 +1,7 @@
+define(["./views/appView", "util/loadCss", "lib/backbone", "lib/underscore", "lib/jquery.tmpl"], 
+
+function (AppView, loadCss) {
+	//loadCss("todos");
+	var app = new AppView();
+	return {};
+});
