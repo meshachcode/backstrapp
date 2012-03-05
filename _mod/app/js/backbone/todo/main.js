7afc083b2912b5e70e20529c6d4ac2d7066542f1
meshachj <meshachjackson@gmail.com>
2012-03-04 18:51:13 -0600

    updating file paths to relative

--- _mod/app/js/backbone/todo/main.js
+++ _mod/app/js/backbone/todo/main.js
@@ -1,10 +1,7 @@
-define(["./views/appView",
-        "util/loadCss",
-         "app/js/lib/backbone.js",
-         "app/js/lib/underscore.js",
-         "app/js/lib/jquery.tmpl.js"],
-    	function (AppView, loadCss) {
-			//loadCss("todos");
-    	    var app = new AppView();
-    	    return {};
-    	});
+define(["./views/appView", "util/loadCss", "lib/backbone", "lib/underscore", "lib/jquery.tmpl"], 
+
+function (AppView, loadCss) {
+	//loadCss("todos");
+	var app = new AppView();
+	return {};
+});
