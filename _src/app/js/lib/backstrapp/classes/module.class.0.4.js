a34ff0813278c9f2def7f2777006f85802cf83a4
Meshach Jackson <meshachjackson@gmail.com>
2012-03-24 04:20:50 -0500

    getting accordion to render slides

--- _src/app/js/lib/backstrapp/classes/module.class.0.4.js
+++ _src/app/js/lib/backstrapp/classes/module.class.0.4.js
@@ -10,15 +10,13 @@ define(['util', 'backbone', '../models/module.model.0.1'], function (util, Backb
 	var ModuleClass = Backbone.View.extend({
 
 		initialize: function (config) {
+			console.log('ModuleClass', config);
 			// init the model here, so that it's a unique instance every time
 			this.model = new ModuleModel(config);
 			// This, effectively 'starts' your module. 
 			// The params args are set by module.instance.init (module.face)
 			this.model.bind('change:arg', this.processParams, this);
 			this.model.bind('change:html', this.render, this);
-			if (this.defaults) {
-				this.model.set(this.defaults);
-			}
 		},
 
 		render: function () {
